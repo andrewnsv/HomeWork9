47eb0f8caaea494ebae4463c24f5d307e63f0c98
andrewnsv <andrejnizovoj598@gmail.com>
2023-01-03 18:46:08 +0200

    fix

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-const output = document.getElementById('output');
-const input = document.getElementById('input');
-const submit = document.getElementById('submit');
-const clear = document.getElementById('clear');
+const output = document.getElementById("output");
+const input = document.getElementById("input");
+const submit = document.getElementById("submit");
+const clear = document.getElementById("clear");
+const err = document.querySelector(".err");
 
-submit.addEventListener('click', () => {
-    if (input.value !== '') {
-      output.textContent = input.value;
-      input.value = '';
-      input.placeholder = 'Введите сообщение';
-    } else {
-      input.placeholder = 'Заполните это поле';
-    }
-  });
+submit.addEventListener("click", () => {
+  if (input.value !== "") {
+    output.textContent = input.value;
+    input.value = "";
+    input.placeholder = "Введите сообщение";
+    err.classList.remove("errActive");
+  } else {
+    input.placeholder = "Заполните это поле";
+    err.classList.add("errActive");
+  }
+});
 
-clear.addEventListener('click', () => {
-  input.value = '';
-  output.textContent = '';
+clear.addEventListener("click", () => {
+  input.value = "";
+  output.textContent = "";
+  err.classList.remove("errActive");
 });
